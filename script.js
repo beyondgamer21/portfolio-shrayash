eb1b87e04d8a980dc5d84f30f670ea411e38a76e
beyondgamer21 <kaleshreyash940@gmail.com>
2025-02-11 00:13:15 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Dark Mode Toggle
-const themeToggle = document.getElementById("theme-toggle");
-themeToggle.addEventListener("click", () => {
-    document.body.classList.toggle("dark-mode");
-});
-
-// GSAP Animations
-gsap.from(".signature", { opacity: 0, y: -20, duration: 1.5 });
-gsap.from("#hero h2", { opacity: 0, x: -100, duration: 1.2 });
